79307d18cc887ce6034bd7be6b1332a4f32405bd
Swebwite <126206478+Swebwite@users.noreply.github.com>
2024-05-16 11:52:38 +1000

    im trying

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float speed = 6f;
+    private float jumpingPower = 24f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
