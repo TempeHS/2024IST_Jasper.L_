aead6538a16fcbe0b02e8f2a7934125b216e38f7
Swebwite <126206478+Swebwite@users.noreply.github.com>
2024-06-04 10:33:44 +1000

    im stuggling

--- My project/Assets/Scripts/PlayerOneWayPlatform.cs
+++ My project/Assets/Scripts/PlayerOneWayPlatform.cs
@@ -11,7 +11,7 @@ public class PlayerOneWayPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             if (currentOneWayPlatform != null)
             {
